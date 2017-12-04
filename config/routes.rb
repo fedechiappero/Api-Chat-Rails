2784160e00ae0c346e1abef87a0b634512fb83e9
Federico Bit <fedechiappero06@gmail.com>
2017-12-03 21:47:56 -0300

    devise funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: '/api/v1/auth'
   mount ActionCable.server => '/cable'
   scope 'api' do
     namespace :v1 do
       resources :messages, only: [:index, :create]
+      resources :users, only: [:create]
     end
   end
   get '*path', to: redirect('/', path: '?%{path}')
