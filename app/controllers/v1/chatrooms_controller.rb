3ef3ee6c0490c64fca7443e29abdb1ddcf156163
Federico Bit <fedechiappero06@gmail.com>
2017-12-11 20:21:52 -0300

    avance minimo
    se pueden recuperar las conversaciones de una persona.. mejore el insert tambien

--- app/controllers/v1/chatrooms_controller.rb
+++ app/controllers/v1/chatrooms_controller.rb
@@ -3,13 +3,16 @@ class V1::ChatroomsController < V1::BaseController
     expose :chatroom
     
     def index
-        chats = Chatroom.where(user_id1: params[:userLogged])
-        render_api(chats)
+        usr = params[:userLogged]
+        chatrooms = Chatroom.where('user_id1' => User.find(usr.to_i)).or(Chatroom.where('user_id2' => User.find(usr.to_i)))
+        render_api(chatrooms)
     end
 
     def create
-        chatroom.user_id1 = User.find(params[:user_id1])
-        chatroom.user_id2 = User.find(params[:user_id2])
+        usr1 = [params[:user_id1],params[:user_id2]].min
+        usr2 = [params[:user_id1],params[:user_id2]].max
+        chatroom.user1 = User.find(usr1)
+        chatroom.user2 = User.find(usr2)
         chatroom.save 
         render_api(chatroom)
     end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,6 +1,9 @@
 class Chatroom < ApplicationRecord
-    belongs_to :user_id1, class_name: "User", foreign_key: "user_id1"
-    belongs_to :user_id2, class_name: "User", foreign_key: "user_id2"
+    #las claves foraneas daban error SystemStackError (stack level too deep): cuando estaban definidas igual en las puntas
+    belongs_to :user1, class_name: "User", foreign_key: "user_id1"
+    belongs_to :user2, class_name: "User", foreign_key: "user_id2"
 
-    has_many :messages, dependent: :destroy
+    #has_many :messages, dependent: :destroy
+
+    validates :user_id1, :user_id2, presence: true
 end
